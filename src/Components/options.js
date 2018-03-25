b480e2b3fda2755bc953ab6431a2a6b1525bf44c
nphilli3 <nphilli3@gmail.com>
2018-03-25 14:16:20 -0400

    added alt to images

--- src/Components/options.js
+++ src/Components/options.js
@@ -6,21 +6,21 @@ const options = (props) => {
 			<div className="row" id="projects">
 		      <figure className="col-sm-4 offset-2">
 		        <p>Suggestion Box</p>
-		        <a><img src="https://businessfirstfamily.com/wp-content/uploads/2016/09/suggestion-box-improve-business.jpg"/></a>
+		        <a><img alt='' src="https://businessfirstfamily.com/wp-content/uploads/2016/09/suggestion-box-improve-business.jpg"/></a>
 		        </figure>
 		      <figure className="col-sm-4">
 		        <p>Temperature</p>
-		        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTezbJ3VUqWMRJU9Jrh9ydMxuAN6Y_ZtN6IX1v8yZLwhWj1QV6c"/>
+		        <img alt='' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTezbJ3VUqWMRJU9Jrh9ydMxuAN6Y_ZtN6IX1v8yZLwhWj1QV6c"/>
 		      </figure>
 		  	</div>
 		    <div className="row">
 		      <figure className="col-sm-4 offset-2">
 		        <p>Robot</p>
-		        <img src="http://d2rormqr1qwzpz.cloudfront.net/uploads/0/5/33009-boxie_teaser.jpg"/>
+		        <img alt='' src="http://d2rormqr1qwzpz.cloudfront.net/uploads/0/5/33009-boxie_teaser.jpg"/>
 		      </figure>
 		      <figure className="col-sm-4">
 		        <p>Club Information</p>
-		        <img src="https://www.certsi.es/sites/default/files/styles/recuadro_original/public/contenidos/blog/portada_7.png?itok=qJNd5mvr"/>
+		        <img alt='' src="https://www.certsi.es/sites/default/files/styles/recuadro_original/public/contenidos/blog/portada_7.png?itok=qJNd5mvr"/>
 		      </figure>
 	    	</div>
     	</section>

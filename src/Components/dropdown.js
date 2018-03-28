ad196ebd59fe8c68f6ef7bbe036c74e793ab383e
nphilli3 <nphilli3@gmail.com>
2018-03-28 01:37:32 -0400

    minor changes to layout

--- src/Components/dropdown.js
+++ src/Components/dropdown.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 
 export default class Dropdown extends React.Component {
   constructor(props) {
@@ -20,36 +21,54 @@ export default class Dropdown extends React.Component {
 
 render(){
     return (
-      <div className="clearfix">
+      <div>
         <Navbar color="faded" light>
           <NavbarBrand href="/" className="mr-auto">Hilliard IOT</NavbarBrand>
           <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
           <Collapse isOpen={!this.state.collapsed} navbar>
             <Nav navbar className="mr-auto ">
+            <Col sm={{ size: 2, order: 0, offset: 10 }}>
+            <Row>
               <NavItem>
-                <NavLink className="float-right" href="">Team</NavLink>
+                <NavLink  href="">Team</NavLink>
               </NavItem>
+            </Row>
+            <Row>
               <NavItem>
                 <NavLink className="float-right" href="">Current Projects</NavLink>
               </NavItem>
+            </Row>
+            <Row>
               <NavItem>
                 <NavLink className="float-right" href="">Events</NavLink>
               </NavItem>
+            </Row>
+            <Row>
               <NavItem>
                 <NavLink className="float-right" href="">Suggestion Box</NavLink>
               </NavItem>
+            </Row>
+            <Row>
               <NavItem>
                 <NavLink className="float-right" href="">Temperature</NavLink>
               </NavItem>
+            </Row>
+            <Row>
               <NavItem>
                 <NavLink className="float-right" href="">Robot</NavLink>
-              </NavItem>  
+              </NavItem>
+            </Row>
+            <Row>
               <NavItem>
                 <NavLink className="float-right" href="">Club Information</NavLink>
               </NavItem>
+            </Row>
+            <Row>
               <NavItem>
                 <NavLink className="float-right" href="">Contact Us</NavLink>
               </NavItem>
+            </Row>
+              </Col>
             </Nav>
           </Collapse>
         </Navbar>
